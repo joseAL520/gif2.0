5d372046534ea40d965bf9bca0f4bb833fc506ca
jose angel <jose.albelt1817@gmail.com>
2024-05-10 08:32:33 -0500

    diseno y estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+
+//angular core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//modulos
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
+//importacion de componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
