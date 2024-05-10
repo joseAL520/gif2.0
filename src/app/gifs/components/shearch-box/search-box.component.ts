7d07be9c6b0d1c0cdb74096d9e775dd91c5e8c00
jose angel <jose.albelt1817@gmail.com>
2024-05-10 09:38:03 -0500

    viewChildren

--- src/app/gifs/components/shearch-box/search-box.component.ts
+++ src/app/gifs/components/shearch-box/search-box.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
     template: `
 
         <h5> Buscar:</h5>
-        <input type="text"
-               class="form-control"
-               placeholder="buscar Gift..."
+        <input 
+            type="text"
+            class="form-control"
+            placeholder="buscar Gift..."
+            #txtTagInput
+            (keyup.enter)="searchTag()"
         >
 
     `,
 })
 
 export class SearchGiftComponent {
+    
+    @ViewChild('txtTagInput')
+    public tagInput!: ElementRef<HTMLInputElement>;
+
     constructor() { }
 
+    searchTag(){
+        const newTag = this.tagInput.nativeElement.value;
+
+        console.log(newTag)
+    }
    
 }
